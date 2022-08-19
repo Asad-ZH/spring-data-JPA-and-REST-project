8fe3415ed0a7bdbdd0511bf7227a29fd9cbb7dc9
asad <asadzahid1479@gmail.com>
2022-08-19 16:18:09 +0500

    copy file from rest curd apis with hibernate

---
+++ src/main/java/com/example/springrestapis/SpringRestApIsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApIsApplication.class, args);
+    }
+
+}
