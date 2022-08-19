5dacfaa6fd7bc77740cb5f8bbb3f19451aeb7ec9
asad <asadzahid1479@gmail.com>
2022-08-19 16:52:57 +0500

    spring data rest modification completes

--- src/main/java/com/example/springrestapis/dao/EmployeeRepository.java
+++ src/main/java/com/example/springrestapis/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.example.springrestapis.dao;
 
 import com.example.springrestapis.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 
